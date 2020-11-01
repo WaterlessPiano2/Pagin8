28f2f83d4e4d354725675c4274b1012d404c445e
Chad - ubuntu <scderya@gmail.com>
2020-11-01 22:06:37 +0000

    lights off, dark mode on
    (cherry picked from commit fc894b1fadeaa41790e1b85bf604b717765d6ae2)

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,23 @@ import { store } from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
 
 ReactDOM.render(
   <Router>
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <React.StrictMode>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </React.StrictMode>
+    </ThemeProvider>
   </Router>,
   document.getElementById("root")
 );
